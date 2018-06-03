2b58c74be3455304d1131cd45da92be36d9170a9
Jordan  Haddadi <jordanhaddadi@gmail.com>
2018-06-03 13:46:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
 function logShout(string) {
-  console.log (string.toUpperCase())
+  console.log (string.toUpperCase());
 }
 
 function logWhisper(string) {
-  console.log (string.toLowerCase())
+  console.log (string.toLowerCase());
 }
 
 // string = "Hey how are you Grandma?"
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-    return "I can't hear you!"
+    return "I can't hear you!";
   }
   
   if (string.toUpperCase() === string) {
-    return "YES INDEED!"
+    return "YES INDEED!";
   }
   
   if ("I love you, Grandma." ===  string) {
-    return "I love you, too." 
+    return "I love you, too." ;
   }
   
 }
